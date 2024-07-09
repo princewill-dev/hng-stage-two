b160f68e4b4b3cdbcb671d18a369255e315c8004
Princewill Ezekude <ezekudep@gmail.com>
2024-07-09 15:24:54 +0100

    fixed urls

--- api/urls.py
+++ api/urls.py
@@ -5,8 +5,8 @@ urlpatterns = [
     path('test/', IndexView.as_view(), name='index'),
     path('auth/register', RegisterView.as_view(), name='register'),
     path('auth/login', LoginView.as_view(), name='login'),
-    path('users/:<str:userId>', UserDetailView.as_view(), name='user-detail'),
+    path('users/<str:userId>', UserDetailView.as_view(), name='user-detail'),
     path('organisations', OrganisationView.as_view(), name='organisations'),
-    path('organisations/:<str:orgId>', OrganisationDetailView.as_view(), name='organisation-detail'),
-    path('organisations/:<str:orgId>/users', AddUserToOrganisationView.as_view(), name='add-user-to-organisation'),
+    path('organisations/<str:orgId>', OrganisationDetailView.as_view(), name='organisation-detail'),
+    path('organisations/<str:orgId>/users', AddUserToOrganisationView.as_view(), name='add-user-to-organisation'),
 ]
